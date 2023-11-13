a9e41ae623a1ecf67001375dad0e0f57a6376ec1
jechang-park <wpckddl1124@naver.com>
2023-11-14 02:20:20 +0900

    Updates

---
+++ precache-manifest.df529cc7a4ddd3d24226534ce01bd4f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1a4d15ec7c38b066f77842d317b021",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5282edcb4b7889f1ce17",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "bdf063cd53a161d3396d",
+    "url": "/static/css/main.7f5de4e8.chunk.css"
+  },
+  {
+    "revision": "5282edcb4b7889f1ce17",
+    "url": "/static/js/2.a2ca5135.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.a2ca5135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf063cd53a161d3396d",
+    "url": "/static/js/main.c8a2cf3e.chunk.js"
+  },
+  {
+    "revision": "3e7d871e57f4565dd183",
+    "url": "/static/js/runtime-main.ffac27b6.js"
+  }
+]);
