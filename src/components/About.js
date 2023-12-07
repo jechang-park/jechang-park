cc4662a7d692d269ddb41c53b06f29dfaefac8cb
jechang-park <wpckddl1124@naver.com>
2023-12-07 15:52:31 +0900

    1

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
-import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
-import { Next } from "react-bootstrap/esm/PageItem";
+
 
 class About extends Component {
   render() {
@@ -12,7 +8,6 @@ class About extends Component {
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
-      var hello = this.props.resumeBasicInfo.description_header;
       var about = this.props.resumeBasicInfo.description;
     }
 
